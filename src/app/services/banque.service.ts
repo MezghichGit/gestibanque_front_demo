849cc1a9b2d0da4c159118ad3ffd8e72ac6438f7
Mohamed Amine MEZGHICH <git@mezghich.com>
2024-02-20 11:16:09 +0100

    variable d'environnement

--- src/app/services/banque.service.ts
+++ src/app/services/banque.service.ts
@@ -1,36 +1,38 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.development';
 @Injectable({
   providedIn: 'root'
 })
 export class BanqueService {
 
+
   constructor(private http:HttpClient) { }
 
   getBanques()
   {
-     return this.http.get("http://127.0.0.1:8080/banques/"); // consommation d'une api rest
+     return this.http.get(environment.baseUrl+"banques/"); // consommation d'une api rest
   }
 
   addBanque(banque:any)
   {
-     return this.http.post("http://127.0.0.1:8080/banques/",banque); // consommation d'une api rest
+     return this.http.post(environment.baseUrl+"banques/",banque); // consommation d'une api rest
   }
 
   deleteBanque(banque:any)
   {
-     return this.http.delete("http://127.0.0.1:8080/banques/"+banque.id); // consommation d'une api rest
+     return this.http.delete(environment.baseUrl+"banques/"+banque.id); // consommation d'une api rest
   }
 
   getBanque(id:any)
   {
     console.log(id);
-     return this.http.get("http://127.0.0.1:8080/banques/"+id); // consommation d'une api rest
+     return this.http.get(environment.baseUrl+"banques/"+id); // consommation d'une api rest
   }
 
   updateBanque(banque:any)
   {
 
-     return this.http.put("http://127.0.0.1:8080/banques/",banque); // consommation d'une api rest
+     return this.http.put(environment.baseUrl+"banques/",banque); // consommation d'une api rest
   }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  baseUrl : 'http://127.0.0.1:8080/',
+};
